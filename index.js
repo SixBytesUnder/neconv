82eca862b86582df20eff08734c4b590f3a12cd9
SixBytesUnder <wojtek.kosinski@gmail.com>
2018-02-03 13:31:50 +0000

    Code cleanup

--- index.js
+++ index.js
@@ -9,17 +9,11 @@ const ora = require('ora');
 const fs = require('fs');
 var files = [];
 
-
 program
 	.version('1.2.0')
 	.option('-a, --all', 'Process all txt files in current directory')
 	.option('-f, --file <file>', 'Process only given file')
 	.parse(process.argv);
-
-function getFile() {
-    var filePath = __dirname + "/image1" + index + ".png";
-    return fs.readFileAsync(filePath);
-}
 
 function getFiles(dir) {
 	var filesRaw = fs.readdirSync(dir);
@@ -35,10 +29,10 @@ function getFiles(dir) {
     recode(files);
 }
 
-let recode = function() {
+function recode() {
     if (files.length > 0) {
         var fileName = files.shift();
-        const spinner = ora('File ' + path.basename(fileName) + ' processing...').start();
+        const spinner = ora(`File ${path.basename(fileName)} processing...`).start();
         var fileSingle = fs.readFileSync(fileName);
         var data = new Buffer(fileSingle, "ascii");
         var translated = encoding.convert(data, "UTF-8", "CP1250");
@@ -46,10 +40,10 @@ let recode = function() {
         
         fs.writeFile(fileName, converted, function(err) {
             if (err) {
-                spinner.fail('File ' + path.basename(fileName) + ' failed.');
+                spinner.fail(`File ${path.basename(fileName)} failed.`);
                 console.log(err);
             } else {
-                spinner.succeed('File ' + path.basename(fileName) + ' DONE!');
+                spinner.succeed(`File ${path.basename(fileName)} DONE!`);
             }
             recode(files);
         });
@@ -60,7 +54,7 @@ let recode = function() {
 
 if (program.all === true) {
 	getFiles('.');
-} else if (program.file != undefined) {
+} else if (program.file !== undefined) {
     files.push(path.resolve(process.cwd(), program.file));
 	recode();
 } else {
